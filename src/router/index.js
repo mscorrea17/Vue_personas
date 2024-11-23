97791168c337abeeac93513cab30c73fa7004236
Michael Correa <trabajosmichaelcorrea@gmail.com>
2024-11-23 10:30:26 -0500

    Correccion de errores

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 
-import EditarComuna from '../components/comunas/EditarComuna.vue';
-import NewComuna from '../components/comunas/NewComuna.vue';
-import Comunas from '../views/Comunas';
-import Home from '../views/Home.vue';
+import EditarComuna from '@/components/Comuna/EditarComuna.vue';
+import NewComuna from '@/components/Comuna/NewComuna.vue';
+import Comunas from '@/views/Comunas.vue';
+import Home from '@/views/HomeView.vue'
 
 
 const routes = [
@@ -28,16 +28,7 @@ const routes = [
     path: '/add-comuna/',
     name: 'NewComuna',
     component: NewComuna,
-  },
-  
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
+  }
 ];
 
 const router = createRouter({
--- src/views/Comunas.vue
+++ src/views/Comunas.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container">
-    <h1 class="text-start">Listado Comunas |
-      <button @click="newComuna()"
-        class="btn btn-success mx-2">
+    <h1 class="text-start">
+      Listado Comunas |
+      <button @click="newComuna()" class="btn btn-success mx-2">
         <font-awesome-icon icon="plus" />
       </button>
     </h1>
@@ -17,18 +17,16 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(comuna,index) in comunas" :key="index">
-          <th scope="row"> {{index+1}}</th>
-          <td>{{comuna.comu_codi}}</td>
-          <td>{{comuna.comu_nomb}}</td>
-          <td>{{comuna.muni_nomb}}</td>
+        <tr v-for="(comuna, index) in comunas" :key="index">
+          <th scope="row">{{ index + 1 }}</th>
+          <td>{{ comuna.comu_codi }}</td>
+          <td>{{ comuna.comu_nomb }}</td>
+          <td>{{ comuna.muni_nomb }}</td>
           <td>
-            <button @click="deleteComuna(comuna.comu_codi)"
-              class="btn btn-danger mx-2">
+            <button @click="deleteComuna(comuna.comu_codi)" class="btn btn-danger mx-2">
               <font-awesome-icon icon="trash" />
             </button>
-            <button @click="editComuna(comuna.comu_codi)"
-              class="btn btn-warning mx-2">
+            <button @click="editComuna(comuna.comu_codi)" class="btn btn-warning mx-2">
               <font-awesome-icon icon="pencil" />
             </button>
           </td>
@@ -39,47 +37,56 @@
 </template>
 
 <script>
-import axios from 'axios'
-import Swal from 'sweetalert2'
-export default {
+import axios from 'axios';
+import Swal from 'sweetalert2';
 
+export default {
   name: 'Comuna',
   data() {
     return {
-      comunas: []
-    }
+      comunas: [],
+    };
   },
   methods: {
     deleteComuna(codigo) {
       Swal.fire({
-        title: `Do you want to delete the Comuna with id ${codigo}?`,
+        title: `Do you want to delete the Comuna with ID ${codigo}?`,
         showCancelButton: true,
         confirmButtonText: 'Delete',
       }).then((result) => {
-        /* Read more about isConfirmed, isDenied below */
         if (result.isConfirmed) {
-          //Swal.fire('Saved!', '', 'success')
-          axios.delete(`http://127.0.0.1:8000/api/comunas/${codigo}`)
-            .then(response => {
+          axios
+            .delete(`http://127.0.0.1:8000/api/comunas/${codigo}`)
+            .then((response) => {
               if (response.data.success) {
-                Swal.fire('Deleted!! ', '', 'success')
-                this.comunas = response.data.comunas
+                Swal.fire('Deleted!', '', 'success');
+                this.comunas = response.data.comunas;
               }
             })
+            .catch((error) => {
+              Swal.fire('Error!', 'There was a problem deleting the Comuna.', 'error');
+              console.error(error); // Depuración
+            });
         }
-      })
+      });
     },
     editComuna(id) {
-      this.$router.push({name: 'EditarComuna', params: { id: `${id}` }})
+      this.$router.push({ name: 'EditarComuna', params: { id: `${id}` } });
     },
     newComuna() {
-      this.$router.push({name: 'NewComuna'});
-    }
+      this.$router.push({ name: 'NewComuna' });
+    },
   },
   mounted() {
     axios
       .get('http://127.0.0.1:8000/api/comunas')
-      .then(response => (this.comunas = response.data.comunas.data))
+      .then((response) => {
+        this.comunas = response.data.comunas.data;
+      })
+      .catch((error) => {
+        Swal.fire('Error!', 'Could not fetch Comunas data.', 'error');
+        console.error(error); // Depuración
+      });
   },
-}
-
+};
+</script>
